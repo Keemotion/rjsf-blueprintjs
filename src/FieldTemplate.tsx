f7acd5e02644158d3961dbffc997b0bbabf0afd9
Benjamin Bours <benjamin.bours@keemotion.com>
2020-09-04 09:30:27 +0200

    Fix bug where help text were not display if not a string type

--- src/FieldTemplate.tsx
+++ src/FieldTemplate.tsx
@@ -3,9 +3,9 @@ import { FieldTemplateProps } from '@rjsf/core';
 import { Classes, FormGroup } from '@blueprintjs/core';
 
 export default function FieldTemplate(props: FieldTemplateProps) {
-  const { children, displayLabel, rawDescription, rawHelp } = props;
+  const { children, displayLabel, rawDescription, rawHelp, uiSchema } = props;
   return (
-    <FormGroup helperText={rawHelp}>
+    <FormGroup className={props.id} helperText={rawHelp || uiSchema['ui:help']}>
       {children}
       {displayLabel && rawDescription && (
         <p
