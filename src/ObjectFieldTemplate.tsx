cdfb497cf4bac2c7b419f490673213002f56fc36
Benjamin Bours <boursbenjamin@gmail.com>
2021-01-27 10:25:20 +0100

    Display divider and title only if set

--- src/ObjectFieldTemplate.tsx
+++ src/ObjectFieldTemplate.tsx
@@ -5,11 +5,20 @@ import { ObjectFieldTemplateProps } from '@rjsf/core';
 const ROOT = 'root';
 
 export default function ObjectFieldTemplate(props: ObjectFieldTemplateProps) {
-  const { idSchema, title, description, properties } = props;
+  const {
+    idSchema,
+    schema: { title },
+    description,
+    properties,
+  } = props;
   return (
     <div className={`schema-${idSchema.$id}`}>
-      <legend className={`${idSchema.$id}-title ${Classes.HEADING}`}>{title}</legend>
-      {idSchema.$id === ROOT && <Divider className="root-divider" style={{ marginBottom: 20 }} />}
+      {title && (
+        <>
+          <legend className={`${idSchema.$id}-title ${Classes.HEADING}`}>{title}</legend>
+          {idSchema.$id === ROOT && <Divider className="root-divider" style={{ marginBottom: 20 }} />}
+        </>
+      )}
       {description && <p>{description}</p>}
       {properties.map((element) => (
         <div key={element.name} className={`property-wrapper property-${element.name}`}>
