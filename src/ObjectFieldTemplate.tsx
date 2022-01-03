6fdbb98e55106464e5f7837de3fcc53965574add
Benjamin Bours <boursbenjamin@gmail.com>
2022-01-03 10:00:09 +0100

    Support noTitle custom schema parameters

--- src/ObjectFieldTemplate.tsx
+++ src/ObjectFieldTemplate.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Divider, Classes } from '@blueprintjs/core';
 import { ObjectFieldTemplateProps } from '@rjsf/core';
+import type { JSONSchema7 } from 'json-schema';
 
 const ROOT = 'root';
 
-export default function ObjectFieldTemplate(props: ObjectFieldTemplateProps) {
+// TODO: make this global
+interface CustomSchema extends JSONSchema7 {
+  noTitle: boolean;
+}
+
+interface Props extends ObjectFieldTemplateProps {
+  schema: CustomSchema;
+}
+
+export default function ObjectFieldTemplate(props: Props) {
   const {
     idSchema,
-    schema: { title },
+    schema: { title, noTitle },
     description,
     properties,
   } = props;
   return (
     <div className={`schema-${idSchema.$id}`}>
-      {title && (
+      {title && !noTitle && (
         <>
           <legend className={`${idSchema.$id}-title ${Classes.HEADING}`}>{title}</legend>
           {idSchema.$id === ROOT && <Divider className="root-divider" style={{ marginBottom: 20 }} />}
         </>
       )}
-      {description && <p>{description}</p>}
+      {description && <p className="description">{description}</p>}
       {properties.map((element) => (
         <div key={element.name} className={`property-wrapper property-${element.name}`}>
           {element.content}
