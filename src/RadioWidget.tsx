c3577078652ba87c5110ab911950f3d58bb1bd46
Benjamin Bours <boursbenjamin@gmail.com>
2022-01-10 17:37:56 +0100

    Add missing name property on radio. Add optional help text for each radio option.

--- src/RadioWidget.tsx
+++ src/RadioWidget.tsx
@@ -1,15 +1,29 @@
 /* eslint-disable no-shadow, react/no-array-index-key */
 import React from 'react';
+import classNames from 'classnames';
 import { WidgetProps } from '@rjsf/core';
 import { RadioGroup, Radio } from '@blueprintjs/core';
+import { UIOptions } from './types';
 
 export default function RadioWidget(props: WidgetProps) {
   const { options, onChange, value, disabled, readonly } = props;
   const _onChange = ({ currentTarget: { value } }: React.FormEvent<HTMLInputElement>) => onChange(value);
+  const { enumOptions } = options as unknown as UIOptions;
   return (
-    <RadioGroup disabled={disabled || readonly} onChange={_onChange} selectedValue={value}>
-      {(options.enumOptions as any).map((it: any) => (
-        <Radio key={it.value} label={it.label} value={it.value} />
+    <RadioGroup name={props.id} disabled={disabled || readonly} onChange={_onChange} selectedValue={value}>
+      {enumOptions.map((it: any) => (
+        <Radio key={it.value} name={props.id} label={it.label} value={it.value}>
+          {it.schema.help && (
+            <div
+              className={classNames({
+                [props.id]: true,
+                'radio-help': true,
+              })}
+            >
+              {it.schema.help}
+            </div>
+          )}
+        </Radio>
       ))}
     </RadioGroup>
   );
